96f02ce25c9602ddc05f762163c522d69c8f4e0a
alanb <efik722life@gmail.com>
2019-08-11 10:42:31 +0900

    list ec2 instance names

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rusoto_core::Region;
-use rusoto_ec2::{DescribeInstancesRequest, Ec2, Ec2Client};
+use rusoto_ec2::{DescribeInstancesRequest, Ec2, Ec2Client, Instance};
 
 fn main() {
     let ec2_client = Ec2Client::new(Region::ApNortheast2);
@@ -7,10 +7,30 @@ fn main() {
 
     match ec2_client.describe_instances(request).sync() {
         Ok(response) => {
-            println!("ok: {:?}", response);
+            // TODO next_token
+            if let Some(reservations) = response.reservations {
+                reservations
+                    .iter()
+                    .flat_map(|reservation| reservation.instances.iter())
+                    .flat_map(|instances| instances.iter())
+                    .for_each(|instance| {
+                        let name = extract_name(instance).map_or("<unnamed>", |name| name);
+                        println!("{:?}", name)
+                    })
+            }
         }
         Err(error) => {
             println!("error: {:?}", error);
         }
+    }
+}
+
+fn extract_name(instance: &Instance) -> Option<&String> {
+    match &instance.tags {
+        Some(tags) => tags
+            .iter()
+            .find(|tag| tag.key == Some("Name".to_string()))
+            .and_then(|tag| (tag.value).as_ref()),
+        None => None,
     }
 }
